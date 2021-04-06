ad50d0618f6852bc27244ab2de564a7954344208
Austin Adams <git@austinjadams.com>
2021-04-06 02:47:11 -0400

    Add basic AES-256 (need to test further)

---
+++ src/aes256/aes256.h
@@ -0,0 +1,17 @@
+#ifndef AES256_H
+#define AES256_H
+
+#include <stdint.h>
+
+// 4 32-bit columns in an AES state
+#define Nb 4
+// 14 rounds in AES-256 cipher
+#define Nr 14
+// 8 words in AES-256 key
+#define Nk 8
+
+#define BLOCK_SIZE (4 * Nb)
+
+extern void aes256(const uint8_t *, const uint8_t *, uint8_t *, int);
+
+#endif
--- src/aes256/main.c
+++ src/aes256/main.c
@@ -1,6 +1,123 @@
+#include <errno.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "aes256.h"
 
-int main(void) {
-    printf("hello, aes256!\n");
+static char *read_to_buf(char *, int *);
+static int write_to_file(char *, char *, int);
+
+// key=$(hexdump -e '16/1 "%02x"' skittles.key )
+// openssl aes-256-ecb -in skittles.png -out skittles.enc.expected -K $key -nopad
+
+int main(int argc, char **argv) {
+    if (argc-1 != 3) {
+        fprintf(stderr, "usage: %s <infile> <keyfile> <outfile>\n", argv[0]);
+        return 1;
+    }
+
+    char *inpath, *keypath, *outpath,
+         *inbuf, *keybuf, *outbuf;
+    inpath = argv[1];
+    keypath = argv[2];
+    outpath = argv[3];
+
+    int in_len, key_len;
+
+    if (!(inbuf = read_to_buf(inpath, &in_len))) {
+        return 1;
+    }
+    if (!(keybuf = read_to_buf(keypath, &key_len))) {
+        free(inbuf);
+        return 1;
+    }
+
+    if (key_len != 32) {
+        fprintf(stderr, "keyfile `%s' is not 256 bits!\n", keypath);
+        free(inbuf);
+        free(keybuf);
+        return 1;
+    }
+
+    if (!(outbuf = calloc(1, in_len))) {
+        perror("calloc");
+        free(inbuf);
+        free(keybuf);
+        return 1;
+    }
+
+    // overall, these casts are probably the safest we could do anywhere
+    // for anything
+    aes256((uint8_t *)inbuf, (uint8_t *)keybuf, (uint8_t *)outbuf, in_len / BLOCK_SIZE);
+
+    free(inbuf);
+    free(keybuf);
+    if (write_to_file(outpath, outbuf, in_len) < 0) {
+        free(outbuf);
+        return 1;
+    }
+
+    free(outbuf);
+    return 0;
+}
+
+static char *read_to_buf(char *path, int *len_out) {
+    FILE *f;
+    if (!(f = fopen(path, "r"))) {
+        perror("fopen");
+        return NULL;
+    }
+
+    int buf_cap = 0, buf_len = 0;
+    char *buf = NULL;
+    int c;
+    errno = 0;
+    while ((c = getc(f)) != EOF) {
+        if (buf_len == buf_cap) {
+            buf_cap += 16;
+            char *new_buf;
+            if (!(new_buf = realloc(buf, buf_cap))) {
+                perror("realloc");
+                free(buf);
+                fclose(f);
+            }
+            buf = new_buf;
+        }
+
+        buf[buf_len] = c;
+        buf_len++;
+    }
+    if (errno) {
+        perror("getc");
+        free(buf);
+        fclose(f);
+        return NULL;
+    }
+
+    if (buf_len < buf_cap) {
+        memset(buf + buf_len, 0, buf_cap - buf_len);
+    }
+    *len_out = buf_cap;
+
+    fclose(f);
+    return buf;
+}
+
+static int write_to_file(char *path, char *buf, int len) {
+    FILE *f;
+    if (!(f = fopen(path, "w"))) {
+        perror("fopen");
+        return -1;
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (putc(buf[i], f) == EOF) {
+            perror("putc");
+            fclose(f);
+            return -1;
+        }
+    }
+
+    fclose(f);
     return 0;
 }
